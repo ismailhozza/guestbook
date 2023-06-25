b4a48647b2b6c4389c20c126cf79b97ae84c31b0
Hussein Ismail <hussein.ismail.m@gmail.com>
2023-06-25 11:14:15 +0300

    Home controller with PHP annotation

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -8,11 +8,18 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/conference', name: 'app_conference')]
+    /**
+     * @Route("/", name="homepage")
+     */
     public function index(): Response
     {
-        return $this->render('conference/index.html.twig', [
-            'controller_name' => 'ConferenceController',
-        ]);
+        return new Response(<<<EOF
+            <html>
+                <body>
+                    <img src="/images/under-construction.gif" />
+                </body>
+            </html>
+            EOF
+        );
     }
 }
