2300d21891cd442c65b88b94e6c0265d78c2d736
Hussein Ismail <hussein.ismail.m@gmail.com>
2024-01-14 16:33:51 +0200

    Fix routing issue

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/conference', name: 'app_conference')]
+    #[Route('/', name: 'app_conference')]
     public function index(): Response
     {
         return $this->render('conference/index.html.twig', [
