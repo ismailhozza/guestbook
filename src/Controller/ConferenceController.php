aa7af9e3a375d351e96cd93623f660a6c9769a56
Hussein Ismail <hussein.ismail.m@gmail.com>
2024-01-14 16:56:06 +0200

    Query param

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -4,16 +4,25 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'app_conference')]
-    public function index(): Response
+    public function index(
+        Request $request
+    ): Response
     {
+        // dump($request);
+        $greet = '';
+        if ($name = $request->query->get('hello')) {
+            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
         return new Response(<<<EOF
             <html>
                 <body>
+                    $greet
                     <img src="/images/under-construction.gif" />
                 </body>
             </html>
